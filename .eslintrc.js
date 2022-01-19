3b4e8602a0542970694839e692eb17d3210a7995
Tanya Samal <solesytto@gmail.com>
2022-01-19 22:46:56 +0300

    init: start rslang task with installing dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'import',
+  ],
+  rules: {
+  },
+};
