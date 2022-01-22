c32d1a63f072a67574219f7f15dfdf6af9729674
Tanya Samal <solesytto@gmail.com>
2022-01-22 21:12:44 +0300

    feat: ankhor navigation on home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,9 +3,19 @@ import HomePage from './home-page.component.html';
 import './home-page.component.scss';
 import { appHeader } from '../../components/header/app.header';
 import { appFooter } from '../../components/footer/app.footer';
+import { ComponentEvent } from '../../../spa/core/coreTypes';
 
 class HomePageComponent extends Component {
-  
+  events = (): ComponentEvent[] =>  [{
+    event: 'click',
+    className: '.more-btn',
+    listener: this.readMore,
+  }];
+
+  readMore(): void {
+    const navTo = <HTMLDivElement>document.querySelector('.about .block-headers');
+    navTo.scrollIntoView();
+  }
 }
 
 export const homePageComponent = new HomePageComponent({
