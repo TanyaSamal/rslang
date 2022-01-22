58a5d6c1d1ee501c03f0b87db2466c126859a4cc
Tanya Samal <solesytto@gmail.com>
2022-01-22 20:00:37 +0300

    feat: open menu in mobile via JS

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import HomePage from './home-page.component.html';
 import './home-page.component.scss';
 import { appHeader } from '../../components/header/app.header';
 import { appFooter } from '../../components/footer/app.footer';
+import { ComponentEvent } from '../../../spa/core/coreTypes';
 
 class HomePageComponent extends Component {}
 
