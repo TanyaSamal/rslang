e2f59de9c714f9e93d16222aae7f3b7b1cacc76c
Tanya Samal <solesytto@gmail.com>
2022-01-24 20:25:54 +0300

    feat: change auth mode between authorization and registration

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -3,8 +3,44 @@ import Auth from './auth.component.html';
 import './auth.component.scss';
 import { appHeader } from '../../components/header/app.header';
 import { appFooter } from '../../components/footer/app.footer';
+import { ComponentEvent } from '../../../spa/core/coreTypes';
 
-class AuthComponent extends Component {}
+const LOGIN = 'login';
+const REGISTRATION = 'registration';
+
+class AuthComponent extends Component {
+  events = (): ComponentEvent[] =>  [{
+    event: 'click',
+    className: '.login-link',
+    listener: this.goToLoginMode,
+  },
+  {
+    event: 'click',
+    className: '.reg-link',
+    listener: this.goToRegistrationMode,
+  }];
+
+  switchMode(mode: string):void {
+    const isLogin = (mode === LOGIN) ? 'block' : 'none';
+    const isReg = (mode === REGISTRATION) ? 'block' : 'none';
+    const liginBlocks = document.querySelectorAll('.authorization-block');
+    liginBlocks.forEach((loginEl: HTMLDivElement) => {
+      loginEl.style.display = isLogin;
+    });
+    const regBlocks = document.querySelectorAll('.registration-block');
+    regBlocks.forEach((regEl: HTMLDivElement) => {
+      regEl.style.display = isReg;
+    });
+  }
+
+  goToLoginMode(): void {
+    this.switchMode(LOGIN);
+  }
+
+  goToRegistrationMode(): void {
+    this.switchMode(REGISTRATION);
+  }
+}
 
 export const authComponent = new AuthComponent({
   selector: 'app-auth',
