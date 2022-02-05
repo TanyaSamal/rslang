82c6fab179e2116e56c2822f74985bf26a624482
dns147-Mac <rarden@ya.ru>
2022-02-05 19:25:06 +0300

    feat: add start window

--- src/app/pages/sprint/sprint.component.ts
+++ src/app/pages/sprint/sprint.component.ts
@@ -1,9 +1,132 @@
-import { Component} from '../../../spa';
+import { Component } from '../../../spa';
 import Sprint from './sprint.component.html';
 import './sprint.component.scss';
 import { appHeader } from '../../components/header/app.header';
+import { activateButton, changeStyleElement, mouseOutElement, mouseOverElement, resetStyleElement } from './utils';
 
-class SprintComponent extends Component {}
+class SprintComponent extends Component {
+  getEventsClick(event: Event): void {
+    if (event.target instanceof Element) {
+      const level1 = event.target.closest('.level1') as HTMLElement;
+      const level2 = event.target.closest('.level2') as HTMLElement;
+      const level3 = event.target.closest('.level3') as HTMLElement;
+      const level4 = event.target.closest('.level4') as HTMLElement;
+      const level5 = event.target.closest('.level5') as HTMLElement;
+      const level6 = event.target.closest('.level6') as HTMLElement;
+      const buttonStartSprint = event.target.closest('.start-sprint') as HTMLElement;
+    
+      if (level1) {
+        resetStyleElement();
+        changeStyleElement(level1);
+        activateButton();
+      }
+
+      if (level2) {
+        resetStyleElement();
+        changeStyleElement(level2);
+        activateButton();
+      }
+
+      if (level3) {
+        resetStyleElement();
+        changeStyleElement(level3);
+        activateButton();
+      }
+
+      if (level4) {
+        resetStyleElement();
+        changeStyleElement(level4);
+        activateButton();
+      }
+
+      if (level5) {
+        resetStyleElement();
+        changeStyleElement(level5);
+        activateButton();
+      }
+
+      if (level6) {
+        resetStyleElement();
+        changeStyleElement(level6);
+        activateButton();
+      }
+
+      if (buttonStartSprint) {
+        // startGameSprint();
+      }
+    }
+  }
+
+  getEventsMouseOver(event: Event): void {
+    if (event.target instanceof Element) {
+      const level1 = event.target.closest('.level1') as HTMLElement;
+      const level2 = event.target.closest('.level2') as HTMLElement;
+      const level3 = event.target.closest('.level3') as HTMLElement;
+      const level4 = event.target.closest('.level4') as HTMLElement;
+      const level5 = event.target.closest('.level5') as HTMLElement;
+      const level6 = event.target.closest('.level6') as HTMLElement;
+    
+      if (level1) {
+        mouseOverElement(level1);
+      }
+
+      if (level2) {
+        mouseOverElement(level2);
+      }
+
+      if (level3) {
+        mouseOverElement(level3);
+      }
+
+      if (level4) {
+        mouseOverElement(level4);
+      }
+
+      if (level5) {
+        mouseOverElement(level5);
+      }
+
+      if (level6) {
+        mouseOverElement(level6);
+      }
+    }
+  }
+
+  getEventsMouseOut(event: Event): void {
+    if (event.target instanceof Element) {
+      const level1 = event.target.closest('.level1') as HTMLElement;
+      const level2 = event.target.closest('.level2') as HTMLElement;
+      const level3 = event.target.closest('.level3') as HTMLElement;
+      const level4 = event.target.closest('.level4') as HTMLElement;
+      const level5 = event.target.closest('.level5') as HTMLElement;
+      const level6 = event.target.closest('.level6') as HTMLElement;
+    
+      if (level1) {
+        mouseOutElement(level1);
+      }
+
+      if (level2) {
+        mouseOutElement(level2);
+      }
+
+      if (level3) {
+        mouseOutElement(level3);
+      }
+
+      if (level4) {
+        mouseOutElement(level4);
+      }
+
+      if (level5) {
+        mouseOutElement(level5);
+      }
+
+      if (level6) {
+        mouseOutElement(level6);
+      }
+    }
+  }
+}
 
 export const sprintComponent = new SprintComponent({
   selector: 'app-sprint',
@@ -12,3 +135,7 @@ export const sprintComponent = new SprintComponent({
   ],
   template: Sprint,
 });
+
+document.addEventListener('click', sprintComponent.getEventsClick);
+document.addEventListener('mouseover', sprintComponent.getEventsMouseOver);
+document.addEventListener('mouseout', sprintComponent.getEventsMouseOut);
---
+++ src/app/pages/sprint/utils.ts
@@ -0,0 +1,40 @@
+export function changeStyleElement(element: HTMLElement): void {
+    const borderColor: string = window.getComputedStyle(element).borderColor;
+
+    element.classList.add('click');
+    element.style.borderRadius = '50%';
+    element.style.backgroundColor = borderColor;
+}
+
+export function resetStyleElement(): void {
+    const allLevel: NodeListOf<Element> = document.querySelectorAll('.level');
+
+    allLevel.forEach((element: HTMLElement) => {
+        element.classList.remove('click');
+        element.style.borderRadius = '0px';
+        element.style.backgroundColor = 'transparent';
+    });
+}
+
+export function mouseOverElement(element: HTMLElement): void {
+    const borderColor: string = window.getComputedStyle(element).borderColor;
+
+    element.style.backgroundColor = borderColor;
+}
+
+export function mouseOutElement(element: HTMLElement): void {
+    const allLevel: NodeListOf<Element> = document.querySelectorAll('.level');
+
+    allLevel.forEach((element: HTMLElement) => {
+        if (!element.classList.contains('click')) {
+            element.style.backgroundColor = 'transparent';
+        }
+    });
+}
+
+export function activateButton(): void {
+    const button = document.querySelector('.start-sprint') as HTMLElement;
+    
+    button.removeAttribute('disabled');
+    button.classList.add('start-sprint-enable');
+}
